79bca66e4afb5fbd3b52b7dcd5c19e0aeeca168c
TerribilisSomnum <51321605+TerribilisSomnum@users.noreply.github.com>
2019-06-20 22:19:19 +0300

    Add files via upload

---
+++ exe_cycles_8.java
@@ -0,0 +1,28 @@
+package by.etc.somnum.chapter_1;
+
+import java.util.*;
+
+public class exe_cycles_8 {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Введите первое и второе число через пробел: ");
+        int numberOne = reader.nextInt();
+        int numberTwo = reader.nextInt();
+
+        Set<Character> uniqueNumberOneSymbols = new HashSet<Character>();
+        for (char c : String.valueOf(numberOne).toCharArray()) {
+            uniqueNumberOneSymbols.add(c);
+        }
+        Set<Character> uniqueNumberTwoSymbols = new HashSet<Character>();
+        for (char c : String.valueOf(numberTwo).toCharArray()) {
+            uniqueNumberTwoSymbols.add(c);
+        }
+        Set<Character> intersection = new HashSet<Character>(uniqueNumberOneSymbols);
+        intersection.retainAll(uniqueNumberTwoSymbols);
+
+        System.out.print("Общие цифры: ");
+        System.out.print(intersection);
+    }
+}
