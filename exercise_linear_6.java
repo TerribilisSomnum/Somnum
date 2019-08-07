d5c2b07e33d8584793b9efe96f95fd26f9cb7d48
TerribilisSomnum <51321605+TerribilisSomnum@users.noreply.github.com>
2019-08-07 19:30:06 +0300

    Update exercise_linear_6.java

--- exercise_linear_6.java
+++ exercise_linear_6.java
@@ -1,14 +1,21 @@
 package by.etc.somnum.chapter_1;
+
+//Для данной области составить линейную программу, которая печатает true, если точка с координатами (х, у)
+//принадлежит закрашенной области, и false — в противном случае: 
+
 import java.util.Scanner;
 
 public class exercise_linear_6 {
+    
     public static void main (String[] args){
 
         Scanner in = new Scanner(System.in);
         System.out.print("Введите координату X: ");
         int x = in.nextInt();
+        
         System.out.print("Введите координату Y: ");
         int y = in.nextInt();
+        
         if (((x >= -2 && x <= 2) && (y >= 0 && y <= 4)) || ((x >= -4 && x <= 4) && (y >= -3 && y <= 0))) {
             System.out.println("true");
         } else {
@@ -16,3 +23,4 @@ public class exercise_linear_6 {
         }
     }
 }
+
