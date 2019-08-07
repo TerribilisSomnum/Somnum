335fdfcd093dc37bd945926f208164ba9e0e5b3b
TerribilisSomnum <51321605+TerribilisSomnum@users.noreply.github.com>
2019-08-07 19:12:21 +0300

    Update exe_cycles_1.java

--- exe_cycles_1.java
+++ exe_cycles_1.java
@@ -1,6 +1,11 @@
 package by.etc.somnum.chapter_1;
 import java.util.Scanner;
 
+//  Напишите программу, где пользователь вводит любое целое положительное число. А программа суммирует
+
+все числа от 1 до введенного пользователем числа.
+
+    
 public class exe_cycles_1 {
     public static void main(String[] args){
 
@@ -15,3 +20,4 @@ public class exe_cycles_1 {
         System.out.println(y);
     }
 }
+
