60bc361e171d9ecc962b29e2de515dd2411ae1a0
TerribilisSomnum <51321605+TerribilisSomnum@users.noreply.github.com>
2019-08-07 19:16:46 +0300

    Update exe_cycles_4.java

--- exe_cycles_4.java
+++ exe_cycles_4.java
@@ -1,14 +1,18 @@
 package by.etc.somnum.chapter_1;
 import java.math.BigInteger;
 
+// Составить программу нахождения произведения квадратов первых двухсот чисел
+
 public class exe_cycles_4 {
+    
     public static void main(String[] args){
+        
         System.out.println(factorial(BigInteger.valueOf(200)).pow(2));
     }
 
     public static BigInteger factorial(BigInteger n) {
+        
         BigInteger result = BigInteger.ONE;
-
         while (!n.equals(BigInteger.ZERO)) {
             result = result.multiply(n);
             n = n.subtract(BigInteger.ONE);
@@ -16,3 +20,4 @@ public class exe_cycles_4 {
         return result;
     }
 }
+
