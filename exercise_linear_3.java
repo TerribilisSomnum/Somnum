8be01f60369990ea4242fd98b14535096c39b1cf
TerribilisSomnum <51321605+TerribilisSomnum@users.noreply.github.com>
2019-08-07 19:27:25 +0300

    Update exercise_linear_3.java

--- exercise_linear_3.java
+++ exercise_linear_3.java
@@ -1,11 +1,18 @@
 package by.etc.somnum.chapter_1;
+
+// Вычислить значение выражения по формуле (все переменные принимают действительные значения):
+
 import java.lang.Math;
 
 public class exercise_linear_3 {
+    
     public static void main(String[] args) {
+        
         double x = 1;
         double y = 2;
+        
         double z = ((((Math.sin(x) + Math.cos(y)) / (Math.cos(x) - Math.sin(y)))) * Math.tan(x * y));
         System.out.println(z);
     }
 }
+
