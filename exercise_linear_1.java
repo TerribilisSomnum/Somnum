73c8463a833867f1dceb0c2f6ffddee179c83da0
TerribilisSomnum <51321605+TerribilisSomnum@users.noreply.github.com>
2019-08-07 19:26:06 +0300

    Update exercise_linear_1.java

--- exercise_linear_1.java
+++ exercise_linear_1.java
@@ -1,11 +1,17 @@
 package by.etc.somnum.chapter_1;
 
+// Найдите значение функции: z = ( (a – 3 ) * b / 2) + c.
+
 public class exercise_linear_1 {
+    
     public static void main(String[] args) {
+        
         int a = 5;
         int b = 3;
         int c = 2;
-        int z = ((a - 3)* b/2) + c;
+        
+        int z = ((a - 3) * b / 2) + c;
         System.out.println(z);
     }
 }
+
