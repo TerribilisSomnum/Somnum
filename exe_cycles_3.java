0566efa9f1ee3fe37313beeb579d6084f4c2caa4
TerribilisSomnum <51321605+TerribilisSomnum@users.noreply.github.com>
2019-08-07 19:15:02 +0300

    Update exe_cycles_3.java

--- exe_cycles_3.java
+++ exe_cycles_3.java
@@ -3,10 +3,12 @@ package by.etc.somnum.chapter_1;
 // Найти сумму квадратов первых ста чисел.
 
 public class exe_cycles_3 {
+    
     public static void main(String[] args){
+        
         int number = 100;
         int sum = 0;
-        sum = (number * (number + 1) * (2 * number + 1)) / 6;
+        sum = (number * (number + 1) * (2 * number + 1)) / 6; 
     System.out.format("%d",sum);
     }
 }
