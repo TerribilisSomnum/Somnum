1b1fbb3ad170d95556562ea485036bb18a68fd1d
TerribilisSomnum <51321605+TerribilisSomnum@users.noreply.github.com>
2019-08-07 19:18:49 +0300

    Update exe_cycles_5.java

--- exe_cycles_5.java
+++ exe_cycles_5.java
@@ -2,13 +2,18 @@ package by.etc.somnum.chapter_1;
 import java.lang.Math;
 import java.util.Scanner;
 
+//Даны числовой ряд и некоторое число е. Найти сумму тех членов ряда, модуль которых больше или равен
+//заданному е. Общий член ряда имеет вид:
+
 public class exe_cycles_5 {
+    
     public static void main(String[] args){
 
         System.out.print("Введите число 'e': ");
         Scanner reader = new Scanner(System.in);
         double number = reader.nextDouble();
         double sum = 0;
+        
         for(int i = 1; common_member(i) >= number; i++){
             sum += common_member(i);
         }
@@ -19,3 +24,4 @@ public class exe_cycles_5 {
         return (((1 /(Math.pow(2, number))) + (1 /(Math.pow(3, number)))));
     }
 }
+
