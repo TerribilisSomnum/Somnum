b0157198d624eaca1774c5d6d18f15831c1befe4
TerribilisSomnum <51321605+TerribilisSomnum@users.noreply.github.com>
2019-08-07 19:22:13 +0300

    Update exercise_branching_1.java

--- exercise_branching_1.java
+++ exercise_branching_1.java
@@ -1,20 +1,27 @@
 package by.etc.somnum.chapter_1;
 
+//Даны два угла треугольника (в градусах). Определить, существует ли такой треугольник, и если да, то будет ли
+//он прямоугольным.
+
 import java.util.Scanner;
 import static java.lang.System.exit;
 
 public class exercise_branching_1 {
+    
     public static void main(String[] args){
 
         Scanner in = new Scanner(System.in);
         System.out.print("Угол X: ");
         int x = in.nextInt();
+        
         System.out.print("Угол Y: ");
         int y = in.nextInt();
+        
         if (x == 180 || y == 180){
             System.out.println("Такого треугольника не существует");
             exit(0);
         }
+        
         if (x + y + 90 == 180){
             System.out.println("Прямоугольник равносторонний");
         } else {
@@ -22,3 +29,4 @@ public class exercise_branching_1 {
         }
     }
 }
+
