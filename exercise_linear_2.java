0d862746c7e2e9ef9e06f27f5e9e9165b7a25f82
TerribilisSomnum <51321605+TerribilisSomnum@users.noreply.github.com>
2019-08-07 19:26:50 +0300

    Update exercise_linear_2.java

--- exercise_linear_2.java
+++ exercise_linear_2.java
@@ -1,12 +1,17 @@
 package by.etc.somnum.chapter_1;
 
+// Вычислить значение выражения по формуле (все переменные принимают действительные значения):
+
 public class exercise_linear_2 {
-    public static void main(String[] args)
-    {
+    
+    public static void main(String[] args){
+        
         int a = 3;
         int b = 2;
         int c = 1;
-        int z = (int) (((b + (Math.sqrt(Math.pow(b, 2) + 4 * a * c)))/ 2 * c) - (Math.pow(a, 3)) * c + (Math.pow(b, -2)));
+        
+        int z = (int) (((b + (Math.sqrt(Math.pow(b, 2) + 4 * a * c))) / 2 * c) - (Math.pow(a, 3)) * c + (Math.pow(b, -2)));
         System.out.println(z);
     }
 }
+
