a77a6920a69f52e4fe48a5f1b7a3e5511a5949f8
TerribilisSomnum <51321605+TerribilisSomnum@users.noreply.github.com>
2019-08-07 19:15:30 +0300

    Update exe_cycles_2.java

--- exe_cycles_2.java
+++ exe_cycles_2.java
@@ -4,11 +4,15 @@ import java.util.Scanner;
 //Вычислить значения функции на отрезке [а,b] c шагом h: 
 
 public class exe_cycles_2 {
+    
     public static void main(String[] args){
+        
         double a = (-2);
         double b = 10;
+        
         System.out.println("Вам дан отрезок от 0 до 10");
         System.out.print("Введите шаг h: ");
+        
         Scanner reader = new Scanner(System.in);
         double h = reader.nextDouble();
 
